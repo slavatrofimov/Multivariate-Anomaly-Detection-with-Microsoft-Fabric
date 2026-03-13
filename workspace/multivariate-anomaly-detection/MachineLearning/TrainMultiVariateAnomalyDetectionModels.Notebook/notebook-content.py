e6ea670914f53a4d04566b069dde07504fc5532f
@slavatrofimov <you@example.com>
2026-03-13 03:50:52 -0400

    Updated version requirements for key libraries.

--- workspace/multivariate-anomaly-detection/MachineLearning/TrainMultiVariateAnomalyDetectionModels.Notebook/notebook-content.py
+++ workspace/multivariate-anomaly-detection/MachineLearning/TrainMultiVariateAnomalyDetectionModels.Notebook/notebook-content.py
@@ -34,7 +34,7 @@
 # CELL ********************
 
 # Install required packages
-!pip install time-series-anomaly-detector --quiet
+%pip install time-series-anomaly-detector==0.3.0 "mlflow<3.0.0"
 
 # METADATA ********************
 
